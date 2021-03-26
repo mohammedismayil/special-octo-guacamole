24ccf43ef9331d10b7a26aadc4f76e83e95c9564
mohammedismayil <mohammedismayil10@gmail.com>
2021-03-26 22:46:04 +0530

    components-

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-// import  myComponent  from "./myComponent";
+import  myComponent  from "./myComponent";
+import  commentComponent  from "./myComponent";
+
 // var element = <h1>Hello, world!</h1>;
 // const mySecondElement =
 // element
@@ -11,10 +13,22 @@ const myTestComponent = () => {
     </div>
   )
 }
+const comment = {
+  name  : "ismayil" ,
+  comment : "hi da deii epdi iruka"
+};
 function App() {
   return (
-    // element,
-    myTestComponent()
+    // // element,
+    // <div>
+    //   <h1>i am a header</h1>
+    //   i am div inside app </div>
+    <div>
+{/* <Welcome /> */}
+<commentComponent />
+    </div>
+     
+      // myComponent()
     // mySecondElement
   );
 }
--- src/myComponent.js
+++ src/myComponent.js
@@ -1,10 +1,31 @@
-import  React from 'react'
-
-const myComponent = () => {
-  return (
+// so this function may have pass the props - and it always return a single div.
+// inside the div- u can write your own UI, but we cannot give another div below the div inside the functional components.
+// using this functional components we can use it to seperate our complex UI into seperate components .
+//
+function Welcome(props) {
+  return( 
     <div>
-        <p> from second element</p>
+
+<h1>Hello, {props.name} </h1>
+    <h2> hello {props.name} im from h2</h2>
+    <div>
+inside another div - so life is full of divs
+
+
     </div>
-  )
+    </div>
+
+   );
 }
-export default myComponent
+function commentComponent(props){
+return (
+<div>
+ <p> { props.name }</p>
+
+ <p> { props.comment }</p>
+</div>
+);
+
+}
+export default commentComponent
+// export default Welcome
