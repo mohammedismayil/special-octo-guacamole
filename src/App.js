135130384c1b6a93b9a858afa3e88a1368b3883d
mohammedismayil <mohammedismayil10@gmail.com>
2021-03-29 23:32:44 +0530

    changed image paths

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import  myComponent  from "./myComponent";
 import  Another  from "./myAnotherComponent";
 import  Welcome  from "./myComponent";
-
+import firstImage from "./Assets/IMG_0040.JPG"
 // var element = <h1>Hello, world!</h1>;
 // const mySecondElement =
 // element
@@ -17,30 +17,26 @@ const myTestComponent = () => {
 const comment = [{
   "name"  : "ismayil" ,
   "comment" : "hi da deii epdi iruka",
-  image:"{process.env.PUBLIC_URL + '/public/Assets/IMG_0040.JPG'}"
+  image:{firstImage}
 },{
   name  : "ajith" ,
   comment : "ennada panreenga , paathu romba naal aachu",
-  image:"{process.env.PUBLIC_URL + '/public/Assets/IMG_0040.JPG'}"
+  image:{firstImage}
 },{
   name  : "john" ,
   comment : "innumada uyiroda irukeenga",
-  image:"{process.env.PUBLIC_URL + '/public/Assets/IMG_0040.JPG'}"
+  image:{firstImage}
 }] ;
 function App() {
   return (
-    // // element,
-    // <div>
-    //   <h1>i am a header</h1>
-    //   i am div inside app </div>
+   
+  
     <div>
+        {/* <img src={firstImage}></img> */}
       <Another comments = {comment}  />
-{/* <Welcome /> */}
 
     </div>
      
-      // myComponent()
-    // mySecondElement
   );
 }
 
--- src/myAnotherComponent.js
+++ src/myAnotherComponent.js
@@ -1,13 +1,14 @@
+import firstImage from "./Assets/IMG_0040.JPG"
 function Another(props) {
     return( 
       <div>
         {
 props.comments.map(comment => (
     <div>
-      <img src={comment.image}> </img>
+     
          <h1> {comment.name}</h1>
     <h1> {comment.comment}</h1>
-        
+    <img src={firstImage} alt=""> </img>
         </div>
    
 ))
