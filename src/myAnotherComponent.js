6a2cf7abf15288f5099558fe4706f0e84352cc84
mohammedismayil <mohammedismayil10@gmail.com>
2021-03-30 21:34:22 +0530

    import image library from react

--- src/myAnotherComponent.js
+++ src/myAnotherComponent.js
@@ -1,4 +1,5 @@
 import firstImage from "./Assets/IMG_0040.JPG"
+import Image from 'react-bootstrap/Image'
 function Another(props) {
     return( 
       <div>
@@ -8,7 +9,8 @@ props.comments.map(comment => (
      
          <h1> {comment.name}</h1>
     <h1> {comment.comment}</h1>
-    <img src={firstImage} alt=""> </img>
+    {/* <img src={firstImage} alt=""> </img> */}
+    <Image src="{firstImage}" rounded />
         </div>
    
 ))
