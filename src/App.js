39b9c763ded4a320bf39165d615329c5f802931e
mohammedismayil <mohammedismayil10@gmail.com>
2021-04-09 10:11:03 +0530

    tried post method

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@
 // import  Welcome  from "./myComponent";
 // import firstImage from "./Assets/IMG_0040.JPG"
 import './index.css';
-import React, { Component } from 'react';
+import React from 'react';
+// import { request } from 'express';
 // import component
 // var element = <h1>Hello, world!</h1>;
 // const mySecondElement =
@@ -30,18 +31,39 @@ import React, { Component } from 'react';
 //   comment : "innumada uyiroda irukeenga",
 //   image:{firstImage}
 // }] ;
-function print() {
-  console.log("oiii dei")
-}
+// function print() {
+//   console.log("oiii dei")
+// }
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [],
       isLoaded: false,
+      name:"",
+      released_on:"",
     };
   }
-
+  handleChange(event){
+    this.setState({
+      // Computed property names
+      // keys of the objects are computed dynamically
+      [event.target.name] : event.target.value
+    })
+  }
+  insertUser(event){
+    // event.preventDefault();
+    // alert('A name was submitted: ' + this.state.value);
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: JSON.stringify({name:this.state.name.value,released_on:this.state.released_on.value})
+    };
+    fetch('http://localhost:3000/listmovies', requestOptions)
+      .then(response => response.json())
+      // .then(data => this.setState({ postId: data.id }));
+    
+  }
   componentDidMount() {
     fetch('http://localhost:3000/listmovies')
       .then(res => res.json())
@@ -57,10 +79,13 @@ class App extends React.Component {
     const { items } = this.state;
 
     const { isLoaded } = this.state;
+    // const { name } = this.state;
+    // const { released_on } = this.state;
     if (!isLoaded) {
       return <div>Loading ... </div>;
     } else {
       return (
+        <form onSubmit= {this.insertUser.bind(this)}>
         <ul>
           
           {items.movies.map(item => (
@@ -70,9 +95,17 @@ class App extends React.Component {
             </li>
           ))}
         </ul>
+
+         <label>Name</label>
+<input type='text' name="name" value={this.state.value} onChange = {this.handleChange.bind(this)}></input>
+<label>released on</label>
+<input type='text' name="released_on"  value={this.state.value} onChange = {this.handleChange.bind(this)}></input>
+<button type='submit'>insert Movie</button>
+        </form> 
       );
     }
   }
 }
+
 
 export default App;
