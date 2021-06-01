466da74946daa0f18a7e85e1e348dfe8b3d8ec64
mohammedismayil <mohammedismayil10@gmail.com>
2021-06-01 14:05:41 +0530

    changed

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 console.log('in db config');
 const mongoose = require('mongoose');
 const mongoDB = 'mongodb://localhost/jwt';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true },{ useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
