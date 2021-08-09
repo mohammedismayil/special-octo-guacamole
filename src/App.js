7306ae825a5bb0d4c171447f93c133d4f1b9709d
Ismayil <mohammedismayil10@gmail.com>
2021-08-09 23:28:06 +0530

    configuring the whole project for playing with TailwindCSS

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './css/styles.css';
 import './Assets/Images/card-top.jpg'
 function App() {
   return (
-    <div>
-  <p>
-    This a Simple card by tailwindcss. 
-  </p>
-  <img src="./Assets/Images/card-top.jpg"></img>
+    <div className="flex">
+    <div className="max-w-xs rounded overflow-hidden shadow-lg my-2 mx-10"> 
+  <img className="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"></img>
+   <div className="font-bold text-center font-extrabold" >
+   The Coldest Sunset
+   </div>
+   <div className="font-semibold bg-gray-50 text-center">
+   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+   </div>
+    </div>
+    <div className="max-w-xs rounded overflow-hidden shadow-lg my-2 mx-10"> 
+
+    <img className="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"></img>
+      </div>
     </div>
 
+  
   );
 }
 
