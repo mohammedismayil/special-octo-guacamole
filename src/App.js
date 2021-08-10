c54eaf2c20ccaa060dfe545e43a522ce439a127c
Ismayil <mohammedismayil10@gmail.com>
2021-08-10 08:01:53 +0530

    card component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 
 import './css/styles.css';
 import './Assets/Images/card-top.jpg'
+import Card from './Components/Card';
 function App() {
-  return (
-    <div className="flex">
-    <div className="max-w-xs rounded overflow-hidden shadow-lg my-2 mx-10"> 
-  <img className="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"></img>
-   <div className="font-bold text-center font-extrabold" >
-   The Coldest Sunset
-   </div>
-   <div className="font-semibold bg-gray-50 text-center">
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-   </div>
-    </div>
-    <div className="max-w-xs rounded overflow-hidden shadow-lg my-2 mx-10"> 
+  var arr = ["1","2","3"];
+  const list = []
 
-    <img className="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"></img>
-      </div>
+  arr.forEach((product) => {
+    list.push(<Card></Card>)
+  })
+  return (
+   
+
+    <div className="grid grid-cols-3">
+<Card></Card>
+<Card></Card>
+<Card></Card> 
+<Card></Card>
+<Card></Card>
+<Card></Card>   
     </div>
+
+
 
   
   );
