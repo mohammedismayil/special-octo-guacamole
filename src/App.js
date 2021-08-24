2424941efe83678eee398226c662710f363c3d66
ismayil <mohammedismayil10@gmail.com>
2021-08-24 22:48:24 +0530

    nav bar added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Cards from './Components/Cards';
 
 import NavBar from './Components/NavBar';
 import FullscreenLanding from './Components/FullscreenLanding';
+import BurgerRestaurant from './Components/BurgerRestaurant';
 function App() {
 
   return (
@@ -14,9 +15,14 @@ function App() {
    {/* <NavBar></NavBar> */}
 
 {/* <Cards></Cards> */}
-<FullscreenLanding>
+{/* <FullscreenLanding>
   
-</FullscreenLanding>
+</FullscreenLanding> */}
+
+<BurgerRestaurant>
+  
+</BurgerRestaurant>
+
     </div>
 
   );
