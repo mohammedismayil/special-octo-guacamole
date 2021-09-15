7eb2f3c286708fc74b9e67a9f1624103d95f5cd2
ismayil <mohammedismayil10@gmail.com>
2021-09-15 21:41:51 +0530

    adding button

--- src/Components/BurgerRestaurant.js
+++ src/Components/BurgerRestaurant.js
@@ -57,6 +57,14 @@ export default class BurgerRestaurant extends Component {
                 })
                     }
             </div>
+
+
+
+    
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Button
+</button>
+            
             </div>
     
     
