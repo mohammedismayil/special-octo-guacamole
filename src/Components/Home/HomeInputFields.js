988740ec174f259fc398d4cdb30a47629195b8b0
ismayil <mohammedismayil10@gmail.com>
2021-09-20 23:36:42 +0530

    set state not working -- needs to be fixed

--- src/Components/Home/HomeInputFields.js
+++ src/Components/Home/HomeInputFields.js
@@ -4,29 +4,49 @@ import React, { Component } from "react";
 export default class HomeInputFields extends Component {
   constructor(props) {
     super(props);
-    this.state = { posts: [] };
-    // this.setState = () => {
-    //   this.posts = posts;
-    // };
+    
+    this.state = { postData: [] ,name :""};
+    this.setState = () => {
+      this.postData = [];
+    };
+    this.onAddButtonTapped.bind(this);
+    this.onChangeName.bind(this);
   }
   onChangeName = (event) => {
+
+
     this.setState({
-      posts: [
-        {
-          id: Math.random()
-            .toString(36)
-            .replace("0.", "todo" || ""),
-          title: event.target.value,
-        },
-      ],
+      name:event.target.value,
     });
+    console.log(event.target.value)
     // setName(event.target.value);
     // this.props.submitInputData(event.target.name.value);
   };
   onAddButtonTapped = () => {
     // const post = [{ id: 1, title: this.state.posts.title }];
-    this.props.postData.push(this.state.posts);
-    this.props.submitInputData(this.props.postData);
+    this.setState({
+      postData: [
+        {
+          id:1,
+          title:"mahle",
+        },
+        {
+          id:2,
+          title:"kajol",
+        },
+      ],
+    });
+    // this.props.postData.push(this.state.posts);
+    this.props.reloadHomeDataFromInput([
+      {
+        id:1,
+        title:"mahle",
+      },
+      {
+        id:2,
+        title:"kajol",
+      },
+    ]);
   };
   render() {
     return (
@@ -34,10 +54,10 @@ export default class HomeInputFields extends Component {
         <input
           type="name"
           onChange={this.onChangeName}
-          value={this.state.posts.title}
+          value={this.state.title}
         ></input>
         <button onClick={this.onAddButtonTapped}>add</button>
-        {/* <h2>{name}</h2> */}
+        <h2>{this.state.name}</h2>
       </div>
     );
   }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,44 +4,49 @@ import HomeInputFields from "../Components/Home/HomeInputFields";
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    const posts = [];
-    this.state = { posts: [] };
-    this.setState = () => {
-      this.posts = posts;
-    };
+    
+   
+    this.state = { posts: [],anotherPostData: [],name:""};
+    // this.setState = () => {
+    //   this.posts = [];
+    // };
+    this.reloadHomeDataFromInput.bind(this);
+  
   }
-
+  reloadHomeDataFromInput = (postData) => {
+    console.log("ok call back");
+    console.log(postData)
+    
+    
+    this.setState({
+     
+      anotherPostData:postData,
+      name:"ismayil",
+      
+    });
+    
+   
+  }
   render() {
-    const { posts } = this.state;
+    // const { posts } = this.state;
     return (
       <div className="m-auto mx-32 h-64 rounded-lg">
         <div className="text-center text-3xl content-center justify-center">
           Home page
         </div>
         <HomeInputFields
-          submitInputData={this.reloadHomeDataFromInput}
-          postData={this.state.posts}
+          reloadHomeDataFromInput={this.reloadHomeDataFromInput}
+          postData={this.state.anotherPostData}
         ></HomeInputFields>
 
         <ul>
-          {posts.map((post) => (
-            <li key={post.id}>{post.title}</li>
+          {this.state.anotherPostData.map((post) => (
+            <li key={post.id}>{post.id}{post.title}</li>
           ))}
         </ul>
-        <ul>
-        
-            <li>joj</li>
-         
-        </ul>
+        <h2>{this.state.name}</h2>
       </div>
     );
   }
-  reloadHomeDataFromInput = (inputData) => {
-    console.log("ok call back");
-    
-    this.setState({
-      posts: inputData,
-    });
-    console.log(this.state.posts);
-  };
+  
 }
