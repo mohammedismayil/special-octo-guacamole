3e80e3ee5afbe3ae405cebc485ca250220450131
ismayil <mohammedismayil10@gmail.com>
2021-09-22 23:18:41 +0530

    this is what happens when u dont read any documentation and directly get into the project - damn these things are ugly as fuck

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,18 +11,21 @@ export default class Home extends Component {
     //   this.anotherPostData = this.props.anotherPostData;
     // };
     this.reloadHomeDataFromInput.bind(this);
+    // this.removeToDo.bind(this);
+  }
+  removeToDo = (id) => {
+
+console.log("deleting a element")
+    this.setState({anotherPostData: this.state.anotherPostData.filter(item => item.id !== id)}
+      ,() => {
+        console.log("after deleting callback");
+      });
   
   }
   reloadHomeDataFromInput = (postData) => {
-    console.log("ok call back");
-    console.log(postData)
-    
-    
     this.setState({
-     
       anotherPostData:postData,
       name:"ismayil",
-      
     });
     
    
@@ -42,24 +45,26 @@ export default class Home extends Component {
         <ul>
           {this.state.anotherPostData.map((post) => (
             <li key={post.id}>
-              <div className={post.isDone ? "line-through" : "none"}>
-                {post.title}
-              </div>
-              <div>
-                <button>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
+              <div className="flex">
+                <div className={post.isDone ? "line-through" : "none"}>
+                  {post.title}
+                </div>
+                <div className="bg-gray-900">
+                  <button onClick={this.removeToDo(post.id)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
             </li>
           ))}
@@ -67,5 +72,7 @@ export default class Home extends Component {
       </div>
     );
   }
+
+
   
 }
