76e929165f59a104cc6a79ee909810e299d67f31
ismayil <mohammedismayil10@gmail.com>
2021-10-03 17:19:08 +0530

    server deploying commit- cleaned up docker overlay storage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -86,6 +86,21 @@ export default class Home extends Component {
                       ></div>
                       <div className="font-bold">{post.title}</div>
                       <div>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M5 13l4 4L19 7"
+                          />
+                        </svg>
+
                         <button onClick={() => this.switchToDo(post.id)}>
                           {post.isDone ? "set as not done" : "set as done"}
                         </button>
