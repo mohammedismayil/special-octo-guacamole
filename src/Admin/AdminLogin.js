bf5fcad6dfffe08bea473930b83fac69d7c6eaa9
mohammedismayil <mohammedismayil10@gmail.com>
2021-12-11 08:50:32 +0530

    again removed node_modules

--- src/Admin/AdminLogin.js
+++ src/Admin/AdminLogin.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
 
 export default class AdminLogin extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
+  onchangeInputs = (name, value) => {
+    // console.log(value.name);
+    this.setState({
+      [name]: value,
+    });
+  };
   render() {
     return (
-      <div>
-        <input type="text" value="email" />
-
-        <input type="text" value="password" />
+      <div className="m-auto w-1/2">
+        <div>
+          <input
+            type="text"
+            value={this.state.email}
+            name="email"
+            onChange={(e) => this.onchangeInputs(e.target.name, e.target.value)}
+            className="border border-red-500 rounded m-3"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            value={this.state.password}
+            name="password"
+            onChange={(e) => this.onchangeInputs(e.target.name, e.target.value)}
+            className="border border-red-500 rounded m-3"
+          />
+        </div>
 
         <button>login</button>
       </div>
